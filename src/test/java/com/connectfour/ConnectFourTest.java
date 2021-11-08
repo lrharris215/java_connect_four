f18d79379b8f1966191802d28a88fdd59fa7d2a8
Lane Harris <lrharris215@gmail.com>
2021-11-08 10:58:53 -0500

    Added first test

---
+++ src/test/java/com/connectfour/ConnectFourTest.java
@@ -0,0 +1,13 @@
+package com.connectfour;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConnectFourTest {
+    @Test
+    void practiceTestForSkeleton() {
+        int four = 4;
+        assertEquals(4, four);
+    }
+}
