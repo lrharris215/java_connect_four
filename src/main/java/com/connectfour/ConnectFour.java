b0b84e859cfcaa113c5e536e72771e2f039f3f81
Lane Harris <lrharris215@gmail.com>
2021-11-09 17:37:47 -0500

    Add colors
    I added some ANSI colors to the presenter, so it can print
    the board and discs in color.

--- src/main/java/com/connectfour/ConnectFour.java
+++ src/main/java/com/connectfour/ConnectFour.java
@@ -5,8 +5,14 @@ public class ConnectFour {
         Display display = new Display();
         display.println("Connect Four!\n");
 
+        Disc testDisc = new Disc("red", '\u25C9');
+        Disc otherDisc = new Disc("blue",'\u25C9' );
         Board board = new Board();
 
-       Presenter.printBoard(display, board);
+        Presenter.printBoard(display, board);
+
+        board.addDisc(testDisc, 0 , 0);
+        board.addDisc(otherDisc, 3 , 4);
+        Presenter.printBoard(display, board);
     }
 }
--- src/main/java/com/connectfour/Presenter.java
+++ src/main/java/com/connectfour/Presenter.java
@@ -1,6 +1,17 @@
 package com.connectfour;
 
 public class Presenter {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+
     public static void printBoard(Displayable display, Board board){
         Discable disc;
 
@@ -17,7 +28,15 @@ public class Presenter {
     }
 
     public static void printDisc(Displayable display, Discable disc){
-        display.print(disc.getSymbol());
+        if(disc.getColor().equals("red")){
+            display.print(ANSI_RED + disc.getSymbol() + ANSI_RESET);
+        }else if (disc.getColor().equals("blue")){
+            display.print(ANSI_BLUE + disc.getSymbol() + ANSI_RESET);
+        }
+        else {
+            display.print(disc.getSymbol());
+        }
+
     }
 
 
