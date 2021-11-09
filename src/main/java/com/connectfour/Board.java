e02a9fbdb22a984e2f2876883fa801c849723ec5
Lane Harris <lrharris215@gmail.com>
2021-11-09 12:12:11 -0500

    Create Board class skeleton
    Board class has a constructor, and the outline of two methods: printBoard and addDisc.

---
+++ src/main/java/com/connectfour/Board.java
@@ -0,0 +1,22 @@
+package com.connectfour;
+
+public class Board {
+    // Board should consist of a matrix of Discs.
+    Disc[][] grid;
+    public Board(){
+        // Standard connect-four is 6x7
+        grid = new Disc[6][7];
+    }
+
+    // methods
+
+    public void addDisc(Disc disc, int row, int col){
+        // adds a disc to a location in the grid.
+        grid[row][col] = disc;
+    }
+
+    public void printBoard(){
+        // prints the board to the terminal
+    }
+
+}
---
+++ src/main/java/com/connectfour/Disc.java
@@ -0,0 +1,7 @@
+package com.connectfour;
+
+public class Disc {
+    public Disc(){
+
+    }
+}
